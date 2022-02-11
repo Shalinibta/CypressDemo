5b9811f9badbc87cb5b664200530740682f0bb7b
Shalinibta <shalini.kurup@cityandguilds.com>
2022-02-11 13:38:06 +0530

    first commit

---
+++ cypress/integration/2-advanced-examples/Test1.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('My First Test' , function()
+
+{
+
+it ("M First Test",function(){
+
+
+    cy.visit("https://www.google.co.in/");
+    cy.get('.gLFyf').type('City&Guilds India')
+
+
+
+
+})
+
+
+
+
+}
+)
